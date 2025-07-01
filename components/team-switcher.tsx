eede55251854f8605e1c8740ca8d9e390b1c57fa
Leo Sperandio <leomartins64@yahoo.com.br>
2025-07-01 09:20:07 -0300

    refactor: change metadata title name

--- components/team-switcher.tsx
+++ components/team-switcher.tsx
@@ -21,7 +21,7 @@ export function TeamSwitcher() {
           </div>
           <div className="grid flex-1 text-left text-sm leading-tight">
             <span className="truncate font-semibold">Proacting</span>
-            <span className="truncate text-xs">Platform</span>
+            <span className="truncate text-xs">Platforma</span>
           </div>
         </SidebarMenuButton>
       </SidebarMenuItem>
